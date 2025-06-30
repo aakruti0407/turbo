5b41b2e01b80624662e3cc96969ebeb15a966888
aakruti0407 <naynapolara@gmail.com>
2025-06-30 11:15:23 +0530

    30/06/25

---
+++ whileloop.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class whileloop {
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new  Scanner(System.in);
+		System.out.println("Enter N :");
+		n=sc.nextInt();
+		while(n>0)
+		{
+			System.out.println("Tops Technologies");
+			n--;
+		}
+	}
+
+}
