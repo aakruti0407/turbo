90131903ede7495153939d6ecf8b8b07ba4cdb42
aakruti0407 <naynapolara@gmail.com>
2025-06-30 11:15:57 +0530

    30/06/25

---
+++ variable.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class variable {
+	
+	public static void main(String[] args) {
+		double a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter A : ");
+		a=sc.nextDouble();
+		System.out.print("Enter B : ");
+		b=sc.nextDouble();
+		c=a+b;
+		System.out.println("Addition : "+c);
+		c=a-b;
+		System.out.println("subtraction : "+c);
+		c=a*b;
+		System.out.println("Multiplication : "+c);
+		c=a/b;
+		System.out.println("Division: "+c);
+	}
+
+}
